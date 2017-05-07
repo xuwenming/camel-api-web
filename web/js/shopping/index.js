aaa62c8d6a725ccd0c39c1f5c4b6e3c88bdbe298
xuwenming <252429711@qq.com>
2017-05-07 12:48:41 +0800

    5.7

---
+++ web/js/shopping/index.js
@@ -0,0 +1,40 @@
+/**
+ * Created by john on 17/5/3.
+ */
+$(function () {
+    //TODO 写我的页面的js业务代码
+
+    $(".all").on("click",function(){
+        var currentClass = $(this).children("span").attr("class");
+        var className = (currentClass == "select") ? "selected" : "select";
+        $(this).children("span").attr("class",className);
+        $(".ui-select span").attr("class",className);
+    })
+
+    $(".ui-select").on("click",function(){
+        var currentClass = $(this).children("span").attr("class");
+        var className = (currentClass == "select") ? "selected" : "select";
+        $(this).children("span").attr("class",className);
+        checkSelectAll();
+    })
+
+
+});
+
+function checkSelectAll()
+{
+    var isSelectAll = true;
+
+    $(".ui-select span").each(function(){
+        if($(this).attr("class") == "select")
+        {
+            $(".all span").attr("class","select");
+            isSelectAll = false;
+        }
+    })
+
+    if(isSelectAll)
+    {
+        $(".all span").attr("class","selected");
+    }
+}
